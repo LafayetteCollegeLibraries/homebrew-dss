519bf2cac56df66dd3a4620b352ad16e4d297872
Deja Jackson <deja.jackson@dejajackson.me>
2021-09-24 16:44:49 -0400

    First attempt at install script

---
+++ csv2vtt.rb
@@ -0,0 +1,19 @@
+class CSV2VTT < Formula
+  desc "Tool for converting the contents of a CSV into a VTT subtitle file."
+  homepage "https://github.com/LafayetteCollegeLibaries/webvtt-cli"
+  url "https://github.com/LafayetteCollegeLibraries/webvtt-cli/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "4d6527dc8c7099766d2773424156f474bac8a3414d29d85e02ed9230ed7cc665"
+  license ""
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "../"
+      system "cmake", "--build", "."
+
+  end
+
+  test do
+    system "#{bin}/csv2vtt"
+  end
