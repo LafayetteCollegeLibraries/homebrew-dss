25032cde3228770cd47aa6dc0bf546a47a27a58d
Deja Jackson <deja.jackson@dejajackson.me>
2021-09-27 11:27:31 -0400

    Install works on my end, but we'll have to see about others

--- csv2vtt.rb
+++ csv2vtt.rb
@@ -1,8 +1,8 @@
 class Csv2vtt < Formula
   desc "Convert the contents of a CSV into a VTT subtitle file."
   homepage "https://github.com/LafayetteCollegeLibraries/homebrew-dss"
-  url "https://github.com/LafayetteCollegeLibraries/webvtt-cli/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "b366d6cdc92fa1c12bc263b6a37a49f99c4322fba015d2aaaa0a9f5a69ac5e21"
+  url "https://github.com/LafayetteCollegeLibraries/webvtt-cli/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "88901df931125d18cacfb98f4ea6c2662b3a0252345682ae9d54cb5d139a0525"
   license ""
 
   depends_on "cmake" => :build
@@ -10,7 +10,7 @@ class Csv2vtt < Formula
   def install
     mkdir "build" do
       system "cmake", "../"
-      system "cmake", "--build", ".", "--config", "Release"
+      system "cmake", "--build", ".", "--config", "Release", "--target", "csv2vtt"
       system "cmake", "--install", "."
     end
   end
